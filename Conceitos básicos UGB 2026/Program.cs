b6f35e0c30c01ba65054d4c6fc3fb95ecac92473
2024102379 <franciscofbmorais05@gmail.com>
2026-02-24 21:11:11 -0300

    Verificando idade do usuário maior de 18 anos

--- Conceitos básicos UGB 2026/Program.cs
+++ Conceitos básicos UGB 2026/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Linq.Expressions;
+
+int idade;
+bool idadevalida = false;
+while(!idadevalida) 
+{
+    Console.WriteLine("Digite sua idade ");
+    try
+    {
+        idade = Convert.ToInt32(Console.ReadLine());
+        idadevalida = true;
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade. ");
+        }
+        else
+        {
+            Console.WriteLine("Você é menor de idade. ");
+        }
+    }
+    catch (FormatException)
+    { 
+
+        Console.WriteLine("O valor informado deve ser algoritmos algébricos. NÚMEROS! ");
+    }
+      
+}
+ 
+
+
+
+
+
