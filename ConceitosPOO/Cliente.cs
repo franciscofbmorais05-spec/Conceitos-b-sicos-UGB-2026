6a723c2b25682372a5f2e9c798e7d159dfffb81e
2024102379 <franciscofbmorais05@gmail.com>
2026-03-03 21:03:28 -0300

    classe cliente com atributos e propriedades (nome e cpf)

---
+++ ConceitosPOO/Cliente.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosPOO
+{
+    public class Cliente
+    {
+        private string _nome;
+        private string _cpf;
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value.Length > 0)
+                    _nome = value;
+                else
+                    throw new Exception(" O nome não pode estar vazio");
+            }
+
+
+
+        }
+        public string Cpf
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                if (value.Length == 11)
+                    _cpf = value;
+                else
+                    throw new Exception("O CPF deve ter exatamente 11 dígitos");
+            }
+        }
+    }
+
+    public class Agencia
+    {
+        private string _numero;
+        public string Numero
+        {
+            get
+            {
+                return _numero;
+            }
+            set
+            {
+                if (value.Length == 11)
+                    _numero = value;
+                else
+                    throw new Exception("O número deve conter DDD + Dígitos");
+            }
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value.Length > 0)
+                    _nome = value;
+                else
+                    throw new Exception("O nome não pode estar vazio");
+            }
+        }
+
+        private string _telefone;
+        public string Telefone
+        {
+            get
+            {
+                return _telefone;
+            }
+            set
+            {
+                if (value.Length == 10)
+                    _telefone = value;
+                else
+                    throw new Exception("O telefone deve conter DDD + Dígitos");
+            }
+        }
+    } 
+
+}
+   
+
---
+++ ConceitosPOO/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceitosPOO;
+
+Console.WriteLine("Hello, World!");
+
+Cliente cliente = new Cliente();
+cliente.Nome = "Francisco";
+Console.WriteLine($"Nome do cliente: {cliente.Nome}");
+
+cliente.Cpf = "19759915758";
+Console.WriteLine($"Cpf do cliente: {cliente.Cpf}");
+
+Agencia agencia = new Agencia();
+agencia.Numero = "24999242979";
+Console.WriteLine($"Numero da agência: {agencia.Numero}");
+
+agencia.Nome = "Flamengo";
+Console.WriteLine($"Nome da agência: {agencia.Nome}");
+
+agencia.Telefone = "2433451705";
+Console.WriteLine($"O telefone da agencia: {agencia.Telefone}");
