e8360eb7902b1572e29030a214215333619a8eeb
franciscofbmorais05-spec <franciscofbmorais05@gmail.com>
2026-03-10 21:49:23 -0300

    Déposito/Saque

---
+++ ConceitosPOO/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosPOO
+{
+    public class Conta
+    {
+        private long _numero;
+        private decimal _saldo;
+
+        public Conta(long numero, decimal saldo)
+        {
+            _numero = numero;
+            _saldo = saldo;
+        }
+
+        public long Numero 
+        {
+            get => _numero;
+            private set 
+            {
+                _numero = value;
+            }
+        }
+
+        public decimal Saldo
+        {
+            get => _saldo;
+            private set
+            {
+                _saldo = value;
+            }
+        }
+
+        public void Deposito(decimal valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Saque(decimal valor)
+        {
+            if (valor <= _saldo)
+            {
+                _saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque");
+            }
+
+        }
+    }
+}
--- ConceitosPOO/Program.cs
+++ ConceitosPOO/Program.cs
@@ -12,9 +12,6 @@ agencia.Telefone = "2433451705";
 Console.WriteLine($"O telefone da agencia: {agencia.Telefone}");
 
 //Cliente//
-
-
-
 Console.Write("Informe o nome do cliente: ");
 string nome = Console.ReadLine();
 Console.Write("Informe o CPF do cliente: ");
@@ -23,5 +20,21 @@ string cpf = Console.ReadLine();
 Cliente cliente = new Cliente(nome, cpf);
 
 
-Console.WriteLine($"Nome do cliente: {cliente.Nome}");
+Console.WriteLine("Informe o número da conta: ");
+long numero = long.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o saldo inicial da conta {numero}: ");
+decimal saldoinicial = decimal.Parse(Console.ReadLine());
+
+//imprimir os dados da conta
+Conta conta = new Conta(numero, saldoinicial);
+Console.WriteLine($" A conta número {conta.Numero} possui saldo {conta.Saldo}");
+//depositar 1000 na conta
+conta.Deposito(1000);
+//exibir saldo
+Console.WriteLine($"A conta número {conta.Numero} possui saldo {conta.Saldo}");
+//sacar 500 da conta
+conta.Saque(500);
+//exibir saldo
+Console.WriteLine($"A conta número {conta.Numero} possui saldo {conta.Saldo}");
 
